13a4255eb60fa4de4e566b53059fed108953575e
zhoujianwei1985 <zhoujianwei1985@sina.com>
2017-11-08 15:21:06 +0800

    commit

--- src/main/java/com/huaweiair/pojo/csedemo/CsedemoImpl.java
+++ src/main/java/com/huaweiair/pojo/csedemo/CsedemoImpl.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.*;
 import io.servicecomb.provider.pojo.RpcSchema;
 import com.huaweiair.pojo.csedemo.Csedemo;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.CsePojoCodegen", date = "2017-11-08T05:41:35.105Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.CsePojoCodegen", date = "2017-11-04T09:03:45.599Z")
 
 @RpcSchema(schemaId = "csedemo")
 public class CsedemoImpl implements Csedemo {
